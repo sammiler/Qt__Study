2af276662cca562325bbce6dfee12751aa21cc65
sammiler <sammilergood@outlook.com>
2025-03-15 22:08:34 +0800

    update

--- 11 面试第一个Qt项目/WBoard-main/WBoard/Source/core/memcheck.h
+++
@@ -1,5 +0,0 @@
-#if defined(WIN32) && defined(_DEBUG)
-   #include <cstdlib>
-   #define DEBUG_NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
-   #define new DEBUG_NEW
-#endif
--- 12 面试的第2个项目 仅程序的UI设计/shotcut-master/scripts/update-dark.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-convert "$1" -negate -brightness-contrast -11.5 $(echo "$1" | sed 's/light/dark/')
-
